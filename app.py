14553b5ce1146e328b58fc7ecd957ecf47be3d43
adminuser <adminuser@kali.mvm>
2023-09-16 23:07:21 +0530

    updated the routes in app.py

--- app.py
+++ app.py
@@ -21,15 +21,14 @@ blob_creator = AzureBlobCreator(blob_storage_client)
 ## instantitaing fastapi 
 app = FastAPI(title="DataCollection-Server")
 
-
 ## fetching all labels from mongodb 
 @app.get("/labels")
 def fetch_label():
     try:
+        global labels
         collections = mongodb_client['labels']
         results = collections.find()
         documents = [document.get('class_name') for document in results]
-
         response = {"Status": "Success", "Response": {'labels': documents}}
         return JSONResponse(content=response, status_code=200, media_type="application/json")
 
@@ -85,20 +84,30 @@ def single_upload():
 # Image Single Upload Api
 @app.post("/single_upload/")
 async def single_upload(label: str, file: UploadFile = None):
-    label = choices.get(label, False)
-    if file.content_type == "image/jpeg" and label != False:
-        response = blob_creator.upload(
-            container_name=label,
-            file_path=file,
-            blob_name='first.jpeg'
-        )
-        return {"filename": file.filename, "label": label, "container-Response": response}
-    else:
-        return {
-            "ContentType": f"Content type should be Image/jpeg not {file.content_type}",
-            "LabelFound": label,
-        }
+    try:
+        collections = mongodb_client['labels']
+        results = collections.find()
+        documents = [document for document in results]
+
+        class_names = [document.get('class_name') for document in documents]
+        is_label_present = label in class_names
+
+        if file.content_type == "image/jpeg" and is_label_present:
+            response = blob_creator.upload(
+                container_name=label,
+                file_path=file,
+                blob_name='first.jpeg'
+            )
+            return {"filename": file.filename, "label": label, "container-Response": response}
+        else:
+            return {
+                "ContentType": f"Content type should be Image/jpeg not {file.content_type}",
+                "LabelFound": label,
+            }
+
+    except Exception as err:
+        return err 
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
