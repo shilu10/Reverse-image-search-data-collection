a40ae42ad7b97f8784b6d14d632f8d883d356b01
adminuser <adminuser@kali.mvm>
2023-09-18 06:40:28 +0530

    updated the code for data_store

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 
 sas_token = generate_account_sas(
     account_name="tfstate686",
-    account_key=" QxedQzl2GHeH57C5rPtuECF8ROHSU4QUeVrdHOBUEJnf/D9yuX11cwjNSJg2+FJ+5lBIEc11QxvZ+ASt/IqVTQ==",
+    account_key="QxedQzl2GHeH57C5rPtuECF8ROHSU4QUeVrdHOBUEJnf/D9yuX11cwjNSJg2+FJ+5lBIEc11QxvZ+ASt/IqVTQ==",
     resource_types=ResourceTypes(service=True),
     permission=AccountSasPermissions(read=True),
     expiry=datetime.utcnow() + timedelta(hours=1)
@@ -28,3 +28,17 @@ sas_token = generate_account_sas(
 az storage account show-connection-string -g tfstate -n tfstate686
 
 share = ShareClient.from_connection_string(conn_str=conn_str, share_name="myshare")
+
+
+### to generate the sas token(used by azcopy as autenticatin token)
+	 az storage file generate-sas --share-name myshare --account-name tfstate686 --permission rcdw --path reverse_image_search_data --expiry 2037-12-31T23:59:00Z
+
+
+### to copy file from local to fileshare azure;
+	# url:
+		# file_share and directory in the fileshare 
+			/myshare/reverse_image_search_data/
+			
+		https://tfstate686.file.core.windows.net/myshare/reverse_image_search_data/
+
+	azcopy copy 'data.txt' 'https://tfstate686.file.core.windows.net/myshare/reverse_image_search_data/?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-09-18T08:33:39Z&st=2023-09-18T00:33:39Z&spr=https,http&sig=fJ7w2frsgEd696k%2BKV50QEuSbqDQlMyEYBLQ2xx40cY%3D' --recursive
--- src/components/datastore_setup.py
+++ src/components/datastore_setup.py
@@ -6,41 +6,68 @@ from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
 from azure.storage.fileshare import ShareClient
 from io import BufferedReader
+import shutil
 
 
+# abstract class (blueprint)
 class DataStore(ABC):
     def __init__(self):
         pass 
 
     @abstractmethod
-    def download_data(self):
+    def __download_data(self):
         pass 
 
     @abstractmethod
-    def extract_initial_data(self):
+    def __extract_initial_data(self):
         pass 
 
     @abstractmethod
-    def prepare_initial_data(self):
+    def __prepare_initial_data(self):
         pass 
 
     @abstractmethod
-    def create_initial_directories(self):
+    def __sync_initial_data(self):
         pass 
 
-    @abstractmethod
-    def upload_initial_files(self):
-        pass 
+    def run(self):
+        try:
+            self.__download_data()
+            self.__extract_initial_data()
+            self.__prepare_initial_data()
+            self.__sync_initial_data()
+        
+        except Exception as err:
+            return err 
 
 
 class AzureFileShareDataStore(DataStore):
+    """
+    this class, contains a logic, which allows us to copy the initial 
+    training data to the data_store. By default caltech-101 dataset is used.
+    methods:
+        __download_data(private)
+        __extract_initial_data(private)
+        __prepare_initial_data(private)
+        __sync_initial_data(private)
+
+    attrs:
+        dataset_name(dtype: str): name of the initial dataset from kaggle.
+        extraction_path(dtype: str): where to extract the initial dataset.
+        avoid(dtype: List): list of classes, needed to be avoided.
+        data_path(dtype: str): path of the data.
+    """
     def __init__(self):
         self.dataset_name = "imbikramsaha/caltech-101" 
         self.extraction_path = "reverse_image_search_initial_data"
         self.avoid = ["BACKGROUND_Google"]
         self.data_path = self.extraction_path + '/caltech-101/'
 
-    def download_data(self):
+    def __download_data(self):
+        """
+        this method, will download the data from kaggle, and extract it into 
+        the extraction path.
+        """
         try: 
             print("[+] Started downloading the dataset")
             if not os.path.exists(self.extraction_path):
@@ -53,7 +80,10 @@ class AzureFileShareDataStore(DataStore):
         except Exception as err:
             return err 
 
-    def extract_initial_data(self):
+    def __extract_initial_data(self):
+        """
+        this method, will extract the initial data, using gzip and tar.
+        """
         try:
             print("[+] started extracting the dataset") 
             os.system("unzip caltech-101.zip")
@@ -62,11 +92,30 @@ class AzureFileShareDataStore(DataStore):
         except Exception as err:
             return err 
 
-    def prepare_initial_data(self):
-         
-
-    def create_initial_directories(self):
-        pass 
-
-    def upload_initial_files(self):
-        pass 
+    def __prepare_initial_data(self):
+        """
+        this method, used to prepare the data for the data_store, like removing the 
+        unwanted classes, etc.
+        """
+        try:
+            classes = os.listdir(self.data_path)
+            for _class in classes:
+                if _class in self.avoid:
+                    dir_path = self.data_path + '/_class'
+                    shutil.rmtree(dir_path)
+        
+        except Exception as err:
+            return err 
+        
+    def __sync_initial_data(self):
+        """
+        this method, used to sync the data from local storage to the data_store
+        (file_share azure).
+        """
+        try:
+            print('[+] Started the data sync') 
+            os.system(F"azcopy copy 'caltech-101' {os.environ['AZCOPY_URL']} --recursive")
+            print('[+] Completed the data sync to datastore')
+        
+        except Exception as err:
+            return err 
